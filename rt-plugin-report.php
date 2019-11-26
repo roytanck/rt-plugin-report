2fef527d69bf13acff46ef00cd907ec21a4047e2
Roy Tanck <roy.tanck@gmail.com>
2019-11-26 07:47:01 +0100

    More descriptive column name for "tested".

--- rt-plugin-report.php
+++ rt-plugin-report.php
@@ -120,7 +120,7 @@ if ( is_admin() && ! class_exists( 'RT_Plugin_Report' ) ) {
 			echo '<th>' . esc_html__( 'Author', 'plugin-report' ) . '</th>';
 			echo '<th>' . esc_html__( 'Installed version', 'plugin-report' ) . '</th>';
 			echo '<th>' . esc_html__( 'Last update', 'plugin-report' ) . '</th>';
-			echo '<th>' . esc_html__( 'Tested', 'plugin-report' ) . '</th>';
+			echo '<th>' . esc_html__( 'Tested up to WP version', 'plugin-report' ) . '</th>';
 			echo '<th>' . esc_html__( 'Rating', 'plugin-report' ) . '</th>';
 			echo '</tr>';
 
