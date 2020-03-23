a518c05752dbb656b0c941fcff4a9fd01706f0e8
Torsten Landsiedel <torsten.landsiedel@gmx.de>
2020-03-23 21:56:35 +0100

    Add UTF-8 to head
    Fix encoding problems with specifying UTF-8 encoding in head

--- js/plugin-report.js
+++ js/plugin-report.js
@@ -72,7 +72,7 @@ jQuery(document).ready( function( $ ){
 	function rtpr_export_table(){
 		var table_html = $('#plugin-report-table').html();
 		var uri = 'data:application/vnd.ms-excel;charset=UTF-8;base64,';
-		var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>';
+		var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>';
 		var format = function(s, c) {
 			return s.replace(/{(\w+)}/g, function(m, p) {
 				return c[p];
